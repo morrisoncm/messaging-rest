69b8bafc1c71a6e1a5fbff781fbb45214644d0e7
CLinton Morrison <morrison.clinton@gmail.com>
2020-02-15 13:58:01 -0500

    GIT ignore files

--- src/main/java/com/demo/rest/controller/GreetingController.java
+++ src/main/java/com/demo/rest/controller/GreetingController.java
@@ -1,26 +1,25 @@
 package com.demo.rest.controller;
 
-import javax.servlet.http.HttpServletRequest;
-
+import com.demo.rest.domain.Greeting;
+import com.demo.rest.domain.Person;
+import com.demo.rest.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.demo.rest.domain.Greeting;
-import com.demo.rest.domain.Person;
-import com.demo.rest.service.GreetingService;
+import javax.servlet.http.HttpServletRequest;
 
-@RestController 
+@RestController
 @RequestMapping("/greetings")
 public class GreetingController {
 
-	@Autowired
-	private GreetingService greetingService;
-	
-	@PostMapping
-	public Greeting getGreeting(HttpServletRequest httpServletRequest, @RequestBody Person person){
-		return greetingService.getGreeting(httpServletRequest.getLocale(), person);
-	}
+    @Autowired
+    private GreetingService greetingService;
+
+    @PostMapping
+    public Greeting getGreeting(HttpServletRequest httpServletRequest, @RequestBody Person person) {
+        return greetingService.getGreeting(httpServletRequest.getLocale(), person);
+    }
 }
--- src/main/java/com/demo/rest/domain/Greeting.java
+++ src/main/java/com/demo/rest/domain/Greeting.java
@@ -2,21 +2,21 @@ package com.demo.rest.domain;
 
 public class Greeting {
 
-	private String message;
+    private String message;
 
-	public Greeting() {
-		super();
-	}
+    public Greeting() {
+        super();
+    }
 
-	public Greeting(String firstname, String lastname, String message) {
-		this.message = firstname + " " + lastname + " " + message;
-	}
+    public Greeting(String firstname, String lastname, String message) {
+        this.message = firstname + " " + lastname + " " + message;
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public String getMessage() {
+        return message;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
